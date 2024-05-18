a1855d2222a15e053310a6406245a10a6e3b5ed8
originalcoelho <fale.coelho@gmail.com>
2024-05-18 00:37:58 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+import {Slide, SlideNav} from './slide.js'
+
+const slide = new SlideNav('.slide','.slide-wrapper')
+slide.init()
+slide.addArrow('.prev', '.next')
+slide.addControl('.custom-controls')
+
+
